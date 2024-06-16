ccf5c4e7154ded43f97aa4651641939b7cda11b1
tmdwns9112 <tmdwns9112@naver.com>
2024-06-16 21:58:07 +0900

    appspec.yml

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -1,29 +1,18 @@
 # switch.sh
 
-#!/bin/bash
+#!/usr/bin/env bash
 
-# Crawl current connected port of WAS
-CURRENT_PORT=$(cat /home/ubuntu/service_url.inc  | grep -Po '[0-9]+' | tail -1)
-TARGET_PORT=0
+ABSPATH=$(readlink -f $0)
+ABSDIR=$(dirname $ABSPATH)
+source ${ABSDIR}/profile.sh
 
-echo "> Nginx currently proxies to ${CURRENT_PORT}."
+function switch_proxy() {
+    IDLE_PORT=$(find_idle_port)
 
-# Toggle port number
-if [ ${CURRENT_PORT} -eq 8081 ]; then
-    TARGET_PORT=8082
-elif [ ${CURRENT_PORT} -eq 8082 ]; then
-    TARGET_PORT=8081
-else
-    echo "> No WAS is connected to nginx"
-    exit 1
-fi
+    echo "> 전환할 Port: $IDLE_PORT"
+    echo "> Port 전환"
+    echo "set \$service_url http://127.0.0.1:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
 
-# Change proxying port into target port
-echo "set \$service_url http://127.0.0.1:${TARGET_PORT};" | tee /home/ubuntu/service_url.inc
-
-echo "> Now Nginx proxies to ${TARGET_PORT}."
-
-# Reload nginx
-sudo service nginx reload
-
-echo "> Nginx reloaded."
+    echo "> 엔진엑스 Reload"
+    sudo service nginx reload
+}
