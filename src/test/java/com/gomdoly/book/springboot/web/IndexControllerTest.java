02af1ac9398f79a347c47699d6ec00f380e5f11f
tmdwns9112 <tmdwns9112@naver.com>
2024-06-16 16:51:05 +0900

    appspec.yml

--- src/test/java/com/gomdoly/book/springboot/web/IndexControllerTest.java
+++ src/test/java/com/gomdoly/book/springboot/web/IndexControllerTest.java
@@ -22,6 +22,6 @@ class IndexControllerTest {
         //when
         String body = this.restTemplate.getForObject("/",String.class);
         //then
-        assertThat(body).contains("스프링 부트로 시작하는 웹서비스!");
+        assertThat(body).contains("스프링 부트로 시작하는 웹서비스! Ver.5");
     }
 }
