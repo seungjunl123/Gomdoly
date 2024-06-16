3edd31b20f5937724f6f486e9966216ceb3255ae
tmdwns9112 <tmdwns9112@naver.com>
2024-06-16 16:42:36 +0900

    appspec.yml

--- src/main/java/com/gomdoly/book/springboot/Application.java
+++ src/main/java/com/gomdoly/book/springboot/Application.java
@@ -3,11 +3,14 @@ package com.gomdoly.book.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication application = new SpringApplication(Application.class);
+        application.addListeners(new ApplicationPidFileWriter());
+        application.run(args);
     }
 }
